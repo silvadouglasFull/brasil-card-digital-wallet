fe5f3d1a9b348e424cf2a162da1cb2b82ab550ef
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-12-02 11:00:49 -0300

    feat: add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+import { ENV } from 'env';
+
+export default defineConfig({
+    out: './drizzle',
+    schema: './src/db/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: ENV.DATABASE_URL,
+        ssl: false,
+    },
+});
