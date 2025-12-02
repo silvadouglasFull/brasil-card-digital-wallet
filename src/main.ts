ca018070faa90aba0ccda652e3b14d84af35c3b9
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-12-02 11:48:00 -0300

    feat: add vs code config to auto fix error prettier

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { ENV } from '../env';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ENV } from "../env";
+import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(ENV.PORT ?? 3000);
