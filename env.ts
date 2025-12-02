e21a78ece3abfa850ae2d26608eb03eb83147fd7
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-12-02 10:54:29 -0300

    feat: configured env zod validation

---
+++ env.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+const envSchema = z.object({
+    PORT: z
+        .string()
+        .refine(
+            (port) => parseInt(port) > 0 && parseInt(port) < 65536,
+            'Invalid port number',
+        ),
+    DATABASE_URL: z
+        .string()
+        .refine((url) => url.startsWith('postgres'), 'Invalid URL format'),
+});
+
+type Env = z.infer<typeof envSchema>;
+
+export const ENV: Env = envSchema.parse(process.env);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { ENV } from '../env';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(ENV.PORT ?? 3000);
 }
 bootstrap();
