251b190fc6d12cd4d9d393217d94c53b0bf083ad
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-12-02 11:46:36 -0300

    feat implemented user module

--- src/modules/users/entities/index.ts
+++ src/modules/users/entities/index.ts
@@ -1,11 +1,11 @@
-import { baseSchema } from '@/core/database/schema-utils';
-import { boolean, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { baseSchema } from "@/core/database/schema-utils";
+import { boolean, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const users = pgTable('users', {
-    ...baseSchema,
-    fullName: varchar('full_name', { length: 100 }).notNull(),
-    document: varchar('document', { length: 14 }).unique().notNull(),
-    email: varchar('email', { length: 255 }).unique().notNull(),
-    password: varchar('password').notNull(),
-    isActive: boolean('is_active').default(false).notNull(),
+export const users = pgTable("users", {
+  ...baseSchema,
+  fullName: varchar("full_name", { length: 100 }).notNull(),
+  document: varchar("document", { length: 14 }).unique().notNull(),
+  email: varchar("email", { length: 255 }).unique().notNull(),
+  password: varchar("password").notNull(),
+  isActive: boolean("is_active").default(false).notNull(),
 });
--- src/modules/users/repositories/user.repository.interface.ts
+++ src/modules/users/repositories/user.repository.interface.ts
@@ -1,10 +1,10 @@
-import { users } from '@/modules/users/entities';
+import { users } from "@/modules/users/entities";
 
 export type NewUser = typeof users.$inferInsert;
 export type User = typeof users.$inferSelect;
 
 export interface IUserRepository {
-    create(data: NewUser): Promise<User>;
-    findByEmail(email: string): Promise<User | undefined>;
-    findById(id: string): Promise<User | undefined>;
+  create(data: NewUser): Promise<User>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
 }
