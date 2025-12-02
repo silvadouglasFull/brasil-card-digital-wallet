926b87dcbc66b24fd1ae7d1df968298393d8cfef
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-12-02 11:46:16 -0300

    feat: created basaic schema

--- src/core/database/schema-utils.ts
+++ src/core/database/schema-utils.ts
@@ -1,8 +1,8 @@
-import { timestamp, uuid } from 'drizzle-orm/pg-core';
+import { timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const baseSchema = {
-    id: uuid('id').defaultRandom().primaryKey(),
-    createdAt: timestamp('created_at').defaultNow().notNull(),
-    updatedAt: timestamp('updated_at').defaultNow().notNull(),
-    deletedAt: timestamp('deleted_at'),
+  id: uuid("id").defaultRandom().primaryKey(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  deletedAt: timestamp("deleted_at"),
 };
